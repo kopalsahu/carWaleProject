39a9b1606d761d57cba6c8450c0e84db82beee79
Akash <Akash@SL-SP-LAP-0377.local>
2019-06-17 16:04:24 +0530

    Updates

---
+++ precache-manifest.d5344972d6bdefca99435a71666e6ed9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9de9c893295057ba0ddf",
+    "url": "/carWaleProject/static/js/runtime~main.9de9c893.js"
+  },
+  {
+    "revision": "a4bf233747396f62406d",
+    "url": "/carWaleProject/static/js/main.a4bf2337.chunk.js"
+  },
+  {
+    "revision": "e990ccfaedfecdf9af24",
+    "url": "/carWaleProject/static/js/1.e990ccfa.chunk.js"
+  },
+  {
+    "revision": "a4bf233747396f62406d",
+    "url": "/carWaleProject/static/css/main.8306bc46.chunk.css"
+  },
+  {
+    "revision": "9a6a883541d6e84de6fb4daf199f63ed",
+    "url": "/carWaleProject/index.html"
+  }
+];
